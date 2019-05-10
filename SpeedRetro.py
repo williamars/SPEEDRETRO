4ea7361b56f60330b230164c83580ff36a380164
William Augusto Reis da Silva <49211518+williamars@users.noreply.github.com>
2019-05-10 09:34:57 -0300

    Velocidade

--- SpeedRetro.py
+++ SpeedRetro.py
@@ -191,7 +191,7 @@ bullets = pygame.sprite.Group()
 x = 0
 y = 0
 # Cria 8 meteoros e adiciona no grupo meteoros
-for i in range(100):
+for i in range(10):
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
@@ -265,21 +265,15 @@ try:
         screen.blit(background, background_rect_cima)
         screen.blit(background, background_rect)
         all_sprites.draw(screen)
-<<<<<<< HEAD
-        if background_rect.y > HEIGHT*2:
-            background_rect.y = 800
-            background_rect_cima.y -= 800
-=======
+
         if background_rect.y>=HEIGHT:
             background_rect.y=0
             background_rect_cima.y=-HEIGHT
->>>>>>> 3a871d30780bd0ce3b3804f9f4395d43b6eb0a17
+
         
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
-#        if background_rect.y>HEIGHT*2:
-#            background_rect.y=800
-#            background_rect_cima.y-=800
+
         
 finally:
     
