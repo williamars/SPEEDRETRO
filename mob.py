532eba30b22c9354e075755e8444db0d7ea591c4
William Augusto Reis da Silva <49211518+williamars@users.noreply.github.com>
2019-05-16 15:57:21 -0300

    excluindo erro

--- mob.py
+++ mob.py
@@ -58,7 +58,7 @@ class Mob(pygame.sprite.Sprite):
         
         # Se o meteoro passar do final da tela, volta para cima
         if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
-<<<<<<< HEAD
+
             i=random.randrange(0,10)               # Sorteia uma faixa para aparecer carrinhos
             if i <=2:
                 self.rect.x = 100
@@ -71,23 +71,6 @@ class Mob(pygame.sprite.Sprite):
             elif i <=10:
                 self.rect.x = 455
 
-=======
-
-           
-            i=random.randrange(0,10)
-
-            if i <= 2:
-                self.rect.x = 100
-            elif i <= 4:
-                self.rect.x = 195
-            elif i <=6:
-                self.rect.x = 280
-            elif i<=8:
-                self.rect.x = 365
-            elif i <=10:
-                self.rect.x = 455
-            
->>>>>>> 3113e4264ae04087733883d7e37efd30d03a0f65
             self.rect.y = random.randrange(-100, -40)
             self.speedx = random.randrange(-3, 3)
             self.speedy = random.randrange(10, 15)
