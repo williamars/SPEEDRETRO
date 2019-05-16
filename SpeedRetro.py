daf1c5f52e99a29566424dfad7c1b7024ff8d70c
William Augusto Reis da Silva <49211518+williamars@users.noreply.github.com>
2019-05-16 17:10:38 -0300

    separando arquivo de novo

--- SpeedRetro.py
+++ SpeedRetro.py
@@ -26,109 +26,17 @@ from mob import Mob
 # Importanto arquivo do outro tiro
 from bullet2 import Bullet2
 
-#Importando arquivo da classe
+# Importando arquivo da classe
 from coin import Coin
 
-#Importando arquivo da classe box
+# Importando arquivo da classe box
 from misterybox import Box
 
-<<<<<<< HEAD
-class Floco(pygame.sprite.Sprite):
-    # Construtor da classe.
-    def __init__(self, flocos_img):
-        
-        # Construtor da classe pai (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-        
-        # Diminuindo o tamanho da imagem.
-        self.image = pygame.image.load(path.join(img_dir, "floco_de_neve.png")).convert()
-        
-        # Diminuindo o tamanho da imagem.
-        self.image = pygame.transform.scale(flocos_img, (35, 38))
-        
-        # Deixando transparente.
-        self.image.set_colorkey(WHITE)
-        
-        # Detalhes sobre o posicionamento.
-        self.rect = self.image.get_rect()
-        
-        # Sorteia um lugar inicial em x
-        posicao_inicial=[100,195,280,365,455] # Posições iniciais dos flocos
-        i=random.randrange(0,5)               # Sorteia uma faixa para aparecer os flocos
-        self.rect.x = posicao_inicial[i]
-        # Sorteia um lugar inicial em y
-        self.rect.y = random.randrange(-100, -40)
-        # Sorteia uma velocidade inicial
-        self.speedx = 0
-        self.speedy = random.randrange(3,10)
-        
-        # Melhora a colisão estabelecendo um raio de um circulo
-        self.radius = int(self.rect.width * 85 / 2)
-            
-    def update(self):
-        
-        self.rect.x += 0
-        self.rect.y += self.speedy
-        
-        if self.rect.right > 520:
-            self.rect.right = 520
-        if self.rect.left < 90:
-            self.rect.left = 90
-        
-        # Se o floco passar do final da tela, volta para cima
-        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
-            posicao_inicial=[100,195,280,365,455]
-            i=random.randrange(0,5)
-            self.rect.x = posicao_inicial[i]
-            self.rect.y = random.randrange(-100, -40)
-            self.speedx = random.randrange(-3, 3)
-            self.speedy = 3
-            
-class Nevasca(pygame.sprite.Sprite):
-    # Construtor da classe.
-    def __init__(self, flocos_img):
-        
-        # Construtor da classe pai (Sprite).
-        pygame.sprite.Sprite.__init__(self)
-        
-        # Diminuindo o tamanho da imagem.
-        self.image = pygame.image.load(path.join(img_dir, "floco_de_neve.png")).convert()
-        
-        # Diminuindo o tamanho da imagem.
-        self.image = pygame.transform.scale(flocos_img, (35, 38))
-        
-        # Deixando transparente.
-        self.image.set_colorkey(WHITE)
-        
-        # Detalhes sobre o posicionamento.
-        self.rect = self.image.get_rect()
-        
-        # Sorteia um lugar inicial em x                      
-        self.rect.x = random.randrange(0,600)
-        # Sorteia um lugar inicial em y
-        self.rect.y = random.randrange(-100, -40)
-        # Sorteia uma velocidade inicial
-        self.speedx = random.randrange(10,20)
-        self.speedy = random.randrange(10,20)
-        
-        # Melhora a colisão estabelecendo um raio de um circulo
-        self.radius = int(self.rect.width * 85 / 2)
-            
-    def update(self):
-        
-        self.rect.x += random.randrange(10,20)
-        self.rect.y += random.randrange(10,20)
-        
-        
-        # Se o floco passar do final da tela, volta para cima
-        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
-            self.rect.x = random.randrange(0,600)
-            self.rect.y = random.randrange(-100, -40)
-            self.speedx = random.randrange(10,20)
-            self.speedy = random.randrange(10,20)
-=======
+# Importando arquivo dos flocos de neve
 from floco import Floco
->>>>>>> 53b84bb86dd1160fc5f7bcf5379e52978aa959d5
+
+# Importando arquivo da nevasca
+from nevasca import Nevasca
 
 # Carrega todos os assets de uma vez só
 def load_assets(img_dir, snd_dir):
@@ -324,7 +232,7 @@ try:
         hits = pygame.sprite.spritecollide(player, flocos, False, False)
         if hits:
             nevasca=pygame.sprite.Group()
-            for i in range(6):
+            for i in range(2):
                 b = Nevasca(assets["flocos_img"])
                 all_sprites.add(b)
                 flocos.add(b)    
